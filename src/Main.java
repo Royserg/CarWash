fe2cca1e7a085d6bee6b0b4f8d3f4c4cca5591a4
Royserg <staniojakub@gmail.com>
2018-10-23 21:12:15 +0200

    Add initial screen showing

--- src/Main.java
+++ src/Main.java
@@ -4,27 +4,94 @@ import java.util.Scanner;
 public class Main {
 
     public static ArrayList<Customer> customers = new ArrayList<>();
+    // create a Scanner object
+    public static Scanner scanner = new Scanner(System.in);
+    // holds logged in Customer
+    public static Customer currentCustomer;
 
+
+    // === main ===
     public static void main(String[] args) {
-        // populate ArrayList with dummy Customers and Owner
+        // populate ArrayList with data
         populateData();
 
-        Scanner scanner=new Scanner(System.in);
-
-        System.out.println("==== SupremeWash ====");
-        System.out.println("1. Register New User");
-        System.out.println("2. Log in");
-        System.out.print("Option: ");
-        // TODO: handle exceptions
-        int num = scanner.nextInt();
-
-//        customers.get(2).
+        // display main screen options
+        showMainScreen();
     }
 
-    public static void populateData() {
-        customers.add(new Customer("Aaron", "AB1234", "+45 512",1234, 200));
+
+    /** Method that populates program's ArrayList with dummy data */
+    private static void populateData() {
+        customers.add(new Customer("Michael", "MC6969", "+45 231",1212, 100));
         customers.add(new Customer("Jakub", "JA9991", "+45 123",0000, 600));
-        customers.add(new Owner("Sam", "SM7771", "+45 783", 9999, 2000));
+        customers.add(new Customer("Sam", "SM7771", "+45 783", 9999, 2000, true));
+        customers.add(new Customer("Anna", "AA2211", "+45 223", 9632, 1200, true));
+        customers.add(new Customer("Mada", "MA6789", "+45 512",1234, 400, true));
+    }
+
+    /** Print Main screen available options */
+    private static void showMainScreen() {
+        System.out.println("==== SupremeWash ====");
+        System.out.println("1. Register a New User");
+        System.out.println("2. Log in");
+        System.out.print("Option: ");
+
+        // collect user input
+        int option;
+
+        do {
+            while (!scanner.hasNextInt()) {
+                System.out.print("Provide a number: ");
+                scanner.next();
+            }
+            option = scanner.nextInt();
+        } while (option < 1 || option > 2);
+
+
+        switch (option) {
+            case 1:
+                showRegistrationScreen();
+                break;
+            case 2:
+                showLoginScreen();
+                break;
+            default:
+                System.out.println("Can't understand you, MASTER");
+        }
+    }
+
+    /** Print Login screen  */
+    public static void showLoginScreen() {
+        System.out.println("==== Login ====");
+        // TODO:
+        // username:  ...
+        // password: ...
+
+        // fail, show loginScreenAgain
+        // success, showUserScreen();
+    }
+
+    /** Print Registration screen */
+    public static void showRegistrationScreen() {
+        System.out.println("==== Registration ====");
+        // TODO:
+        // name, cardPlateNum, phone, ...
+    }
+
+    /** Print User screen */
+    public static void showUserScreen() {
+        String[] options = {"Exit", "Check Balance", "Top-up Balance", "Wash Car", "Generate Statistics"};
+
+        System.out.println("==== Dashboard ====");
+
+        for(int i = 0; i < options.length; i++) {
+            if (i == 4 && !currentCustomer.isAdmin) {
+                continue;
+            }
+            System.out.println(i + ". " + options[i]);
+        }
+
+        // TODO:
     }
 
 }
