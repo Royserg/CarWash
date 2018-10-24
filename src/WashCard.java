2962f1bafb1791759c06fe7fe492d7019a1a1a58
Royserg <staniojakub@gmail.com>
2018-10-24 23:20:43 +0200

    Handle balance attribute and methods

--- src/WashCard.java
+++ src/WashCard.java
@@ -1,11 +1,24 @@
 public class WashCard {
     private String carPlateNum;
+    private double balance;
 
-    public WashCard(String carPlateNum) {
+    public WashCard(String carPlateNum, double balance) {
         this.carPlateNum = carPlateNum;
+        this.balance = balance;
     }
 
+    // getters
     public String getCarPlateNum() {
         return carPlateNum;
     }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    // setters
+    public void setBalance(double newBalance) {
+        this.balance = newBalance;
+    }
+
 }
