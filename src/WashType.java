7092f3eb931127e1ad58cf0283279ce553a3739e
Royserg <staniojakub@gmail.com>
2018-10-24 23:21:25 +0200

    Change prices to int and assign values

--- src/WashType.java
+++ src/WashType.java
@@ -1,6 +1,6 @@
-
 public class WashType {
-      public static double economyPrice;
-      public static double standardPrice;
-      public static double deluxePrice;
+
+    public static int economyPrice = 50;
+    public static int standardPrice = 80;
+    public static int deluxePrice = 120;
 }
